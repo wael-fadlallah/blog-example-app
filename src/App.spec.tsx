f68e57807d3f283ac1e647f41d08a4a1e6256df8
wael-Fadlallah <wael.fudlallah@gmail.com>
2022-03-11 16:39:32 +0400

    making a few test cases

---
+++ src/App.spec.tsx
@@ -0,0 +1,44 @@
+import { it, describe, expect, beforeEach, vi } from "vitest";
+import App from "./App";
+import TodoPanel from "./components/TodoPanel";
+import { shallow } from "enzyme";
+import { render, screen, userEvent, fireEvent } from "./utils/test-utils";
+import "@testing-library/jest-dom/extend-expect";
+
+describe("App Test", () => {
+  let wrapper: any;
+  let panel: any;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+    panel = shallow(
+      <TodoPanel
+        todolist={wrapper.todolist}
+        deleteItem={() => wrapper.deleteItem}
+        checkItem={() => wrapper.checkItem}
+      />
+    );
+  });
+
+  it("Check if the component is mounted", () => {
+    expect(App).toBeTruthy();
+  });
+
+  it("Check if the TodoPanel has been rendered", () => {
+    expect(wrapper.find(TodoPanel).length).toBe(1);
+  });
+
+  it("Check if the button has been rendered", () => {
+    expect(wrapper.find("button").length).toBe(1);
+  });
+
+  it("Check if todo item is added", async () => {
+    render(<App />);
+    const input = screen.getByTestId("todo-input") as HTMLInputElement;
+    fireEvent.change(input, {
+      target: { value: "Testing" },
+    });
+    userEvent.click(screen.getByRole("button"));
+    expect(await screen.getByText(/Testing/i)).toBeInTheDocument();
+  });
+});
